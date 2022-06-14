e8f777768eed0c6f5f54a7ec1d41cba07884f9fa
kbventures <kmb_x@hotmail.com>
2022-06-14 13:31:00 -0400

    Setup CSS

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,32 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-react', '@babel/preset-env'],
-              plugins: ['@babel/plugin-transform-runtime'],
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime'],
+                    },
+                },
             },
-          },
-        },
-      ],
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+        ],
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        template:
-          './public/index.html',
-      }),
+        new HtmlWebpackPlugin({
+            template:
+                './public/index.html',
+        }),
     ],
-  };
+};
+
