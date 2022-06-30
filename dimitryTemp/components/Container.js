7cf57a6c45e92340a8bcc21774c9ed6912f35b0a
kbventures <kmb_x@hotmail.com>
2022-06-30 11:41:47 -0400

    Added Dimitry Directory

---
+++ dimitryTemp/components/Container.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from "./Container.module.css"
+
+// styles.red
+
+export function Container({ children }) {
+    // props.children
+
+    return (
+        <div className={styles.red}>
+            {children}
+        </div>
+    )
+}
+
+
+/*
+    <Container>
+        <Header>
+        <Landing Page>
+        <Footer>
+    </Container>
+
+*/
