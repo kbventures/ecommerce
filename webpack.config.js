017f0187b2b087e5e0a869c40a58f8f91aa715a7
kbventures <kmb_x@hotmail.com>
2022-07-01 12:47:34 -0400

    Removed svg-url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -28,17 +28,6 @@ module.exports = {
                     },
                   ],
             },
-            {
-                test: /\.svg$/,
-                use: [
-                  {
-                    loader: 'svg-url-loader',
-                    options: {
-                      limit: 10000,
-                    },
-                  },
-                ],
-              },
         ],
     },
     plugins: [
