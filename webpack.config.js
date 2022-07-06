94640ab413535828f98808abb34d47fac55977cd
kbventures <kmb_x@hotmail.com>
2022-07-06 15:56:12 -0400

    Fix: Made changes to webpack so refreshes and code changes wouldn't cause 404 error

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index_bundle.js',
+      publicPath: '/'
+    },
     module: {
         rules: [
             {
@@ -30,6 +37,9 @@ module.exports = {
             },
         ],
     },
+    devServer: {
+        historyApiFallback: true,
+      },
     plugins: [
         new HtmlWebpackPlugin({
             template:
