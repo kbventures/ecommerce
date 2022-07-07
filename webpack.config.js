24da7ed57b8933ff601bd32962473ab9d00cf3fa
Dmitry Kulakov <atomeistee@gmail.com>
2022-07-07 19:38:10 +0300

    fix webpack path problem

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,46 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react', '@babel/preset-env'],
-                        plugins: ['@babel/plugin-transform-runtime'],
-                    },
-                },
+  mode: "development",
+  output: {
+    publicPath: "/",
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+              presets: ['@babel/preset-react', '@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
             },
-            {
-                test: /\.css$/,
-                use: [
-                    "style-loader",
-                    {
-                      loader: "css-loader",
-                      options: {
-                        importLoaders: 1,
-                        modules: true,
-                      },
-                    },
-                  ],
-            },
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template:
-                './public/index.html',
-        }),
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:
+          './public/index.html',
+  }),
+  ],
 };
 
