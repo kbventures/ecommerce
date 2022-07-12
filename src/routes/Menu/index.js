dcf0bc3d5c492f8c246f0c47587cedf64db252b3
kbventures <kmb_x@hotmail.com>
2022-07-12 13:52:54 -0400

    Fixed SVG and accessibility

--- src/routes/Menu/index.js
+++ src/routes/Menu/index.js
@@ -5,9 +5,10 @@ import { Link } from "react-router-dom";
 import Testing from "../../../public/assets/Setting.svg"
 import MenuMediumEllipseSvg from "../../../public/assets/Ellipse 20.svg"
 import MenuSmallEllipseSvg from "../../../public/assets/Ellipse 22.svg"
+import { ReactComponent as Logo } from '../../../public/assets/Ellipse 22.svg';
 
 
-const { menu, navigationContainer, navigationContainer_a, navigation, navigation_ul, navigation_li, navigation_profileIcon, navigation_span, navigation_logout,menuMediumEllipse, menuSmallEllipse, menuMediumEllipse_img } = styles
+const { test, menu, navigationContainer, navigationContainer_a, navigation, navigation_ul, navigation_li, navigation_profileIcon, navigation_buyIcon, navigation_heartIcon, navigation_settingIcon, navigation_deliveryIcon, navigation_signOut, navigation_span, navigation_logout, menuMediumEllipse, menuSmallEllipse, menuMediumEllipse_img } = styles
 const url = '../../assets/Setting.svg'
 const ellipseSvg = '../../assets/Ellipse 22.svg'
 
@@ -17,40 +18,45 @@ function Menu() {
             <section className={menu}>
                 <section className={navigationContainer}>
                     <section className={navigation}>
+                        {/* Using IMage tag for static svg */}
                         <section>
-                            {/* <Testing /> */}
+                            <div>
+                                <img src={ellipseSvg} className={test} alt="React Logo" />
+                            </div>
+                            {/* <div className="App">
+                                <Logo />
+                            </div> */}
                             <ul className={navigation_ul}>
-                                <Link className={`${navigation_li} ${navigationContainer_a}`} to="/profile">
-                                <li className={`${navigation_profileIcon}`}><span className={navigation_span}>Profile</span></li>
+                                <Link className={navigation_li} to="/profile">
+                                    <li className={`${navigation_profileIcon}`}><span className={navigation_span}>Profile</span></li>
                                 </Link>
                                 <Link className={`${navigation_li} ${navigationContainer_a}`} to="/orders">
-                                <li className={`${navigation_profileIcon}`}><span className={navigation_span}>Orders</span></li>
+                                    <li className={`${navigation_buyIcon}`}><span className={navigation_span}>Orders</span></li>
                                 </Link>
                                 <Link className={`${navigation_li} ${navigationContainer_a}`} to="/favorite">
-                                <li className={`${navigation_profileIcon}`}><span className={navigation_span}>Favorite</span></li>
+                                    <li className={`${navigation_heartIcon}`}><span className={navigation_span}>Favorite</span></li>
                                 </Link>
                                 <Link className={`${navigation_li} ${navigationContainer_a}`} to="/delivery">
-                                <li className={`${navigation_profileIcon}`}><span className={navigation_span}>Delivery</span></li>
+                                    <li className={`${navigation_deliveryIcon}`}><span className={navigation_span}>Delivery</span></li>
                                 </Link>
                                 <Link className={`${navigation_li} ${navigationContainer_a}`} to="/settings">
-                                <li className={`${navigation_profileIcon}`}><span className={navigation_span}>Settings</span></li>
-                                </Link> 
+                                    <li className={`${navigation_settingIcon}`}><span className={navigation_span}>Settings</span></li>
+                                </Link>
                             </ul>
                         </section>
                         <section className={navigation_logout}>
                             <Link className={navigationContainer_a} to="signout">
-                            Sign Out
+                                Sign Out
                             </Link>
                         </section>
 
 
                         <section className={menuMediumEllipse}>
-                        <img src={ellipseSvg} width="0" />
+                            <img src={ellipseSvg} />
                         </section>
 
-                        <section className={menuSmallEllipse}>
-                            <MenuSmallEllipseSvg width="100"/>
-                        </section>
+                        
+                            <MenuSmallEllipseSvg title="testin" className={menuSmallEllipse} />
                     </section>
                 </section>
 
