1a280506a519ad5ef82fe87c6c5eb902ae9fdff9
kbventures <kmb_x@hotmail.com>
2022-07-13 19:04:00 -0400

    ecommerce: Menu Page CSS Refactoring

--- src/routes/Menu/index.js
+++ src/routes/Menu/index.js
@@ -4,13 +4,14 @@ import Container from "../../components/Container"
 import { Link } from "react-router-dom";
 import Testing from "../../../public/assets/Setting.svg"
 import MenuMediumEllipseSvg from "../../../public/assets/Ellipse 20.svg"
-import MenuSmallEllipseSvg from "../../../public/assets/Ellipse 22.svg"
-import { ReactComponent as Logo } from '../../../public/assets/Ellipse 22.svg';
+import FullSmallEllipseSvg from "../../../public/assets/Ellipse 20.svg"
+import EmptySmallEllipseSvg from "../../../public/assets/Ellipse 22.svg"
+import SemiCircle from "../../../public/assets/Ellipse 19.svg"
 
+const rectangle74 = '../../assets/Rectangle 74.png'
+const rectangle73 = '../../assets/Rectangle 73.png'
 
-const { test, menu, navigationContainer, navigationContainer_a, navigation, navigation_ul, navigation_li, navigation_profileIcon, navigation_buyIcon, navigation_heartIcon, navigation_settingIcon, navigation_deliveryIcon, navigation_signOut, navigation_span, navigation_logout, menuMediumEllipse, menuSmallEllipse, menuMediumEllipse_img } = styles
-const url = '../../assets/Setting.svg'
-const ellipseSvg = '../../assets/Ellipse 22.svg'
+const {semiCircle, emptySmallEllipsePosition, menuLargeImage, rectangle74image, menu, navigationContainer, navigationContainer_a, navigation, navigation_ul, navigation_li, navigation_profileIcon, navigation_buyIcon, navigation_heartIcon, navigation_settingIcon, navigation_deliveryIcon, navigation_signoutIcon, navigation_span, navigation_logout, emptySmallEllipse, fullSmallEllipse, marginLeft15px } = styles
 
 function Menu() {
     return (
@@ -18,14 +19,7 @@ function Menu() {
             <section className={menu}>
                 <section className={navigationContainer}>
                     <section className={navigation}>
-                        {/* Using IMage tag for static svg */}
                         <section>
-                            <div>
-                                <img src={ellipseSvg} className={test} alt="React Logo" />
-                            </div>
-                            {/* <div className="App">
-                                <Logo />
-                            </div> */}
                             <ul className={navigation_ul}>
                                 <Link className={navigation_li} to="/profile">
                                     <li className={`${navigation_profileIcon}`}><span className={navigation_span}>Profile</span></li>
@@ -44,28 +38,25 @@ function Menu() {
                                 </Link>
                             </ul>
                         </section>
-                        <section className={navigation_logout}>
-                            <Link className={navigationContainer_a} to="signout">
+                        <section className={`${navigation_logout} ${navigation_signoutIcon}`}>
+                            <Link className={`${navigationContainer_a} ${marginLeft15px}`} to="signout">
                                 Sign Out
                             </Link>
                         </section>
 
-
-                        <section className={menuMediumEllipse}>
-                            <img src={ellipseSvg} />
-                        </section>
-
-                        
-                            <MenuSmallEllipseSvg title="testin" className={menuSmallEllipse} />
+                        <EmptySmallEllipseSvg title="testin" className={emptySmallEllipse} />
+                        <FullSmallEllipseSvg title="testin" className={fullSmallEllipse} />
                     </section>
                 </section>
 
 
-                <section className="menuLargeImage">
-                    {/* <img src={url} alt="" /> */}
-                    {/* <img className="rectangle74" src="./img/Rectangle 74.png" alt="" /> */}
+                <section className={menuLargeImage}>
+                    <img className={rectangle74image} src={rectangle74} alt="" />
+                    <img src={rectangle73} alt="" />
+                    <EmptySmallEllipseSvg title="testin" class={emptySmallEllipsePosition} />
                 </section>
                 <section className="menuLargeEllipse">
+                    <SemiCircle title="" class={semiCircle} />
                     {/* <img src="./img/Ellipse 19.svg" alt="" /> */}
                 </section>
 
