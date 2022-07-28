52c367a689c634523ad96f6fb6f30411e23a081e
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-07-28 22:58:32 +0800

    fixed checkout page

--- src/routes/Checkout/index.jsx
+++ src/routes/Checkout/index.jsx
@@ -1,71 +1,91 @@
 import React from "react";
 import styles from "./checkout.module.css";
 import Container from "../../components/Container";
-import Button from "../../components/Button/index.jsx";
+import Button from "../../components/Button";
 
 const {
   header,
-  header_title,
+  header_title: headerTitle,
 
-  shipping_info_container,
-  shipping_info_header,
-  shipping_info_main,
-  shipping_info_item,
+  shipping_info_container: shippingInfoContainer,
+  shipping_info_header: shippingInfoHeader,
+  shipping_info_main: shippingInfoMain,
+  shipping_info_item: shippingInfoItem,
 
-  payment_info_container,
-  payment_info_header,
-  payment_info_main,
-  payment_info_option,
-  payment_info_card_wrapper,
+  payment_info_container: paymentInfoContainer,
+  payment_info_header: paymentInfoHeader,
+  payment_info_main: paymentInfoMain,
+  payment_info_option: paymentInfoOption,
+  payment_info_card_wrapper: paymentInfoCardWrapper,
 
-  payment_total,
-  payment_total_amount,
-  payment_button,
+  payment_total: paymentTotal,
+  payment_total_amount: paymentTotalAmount,
 } = styles;
 
 export default function Checkout() {
   return (
     <Container backgroundColor="rgb(245, 245, 248)">
       <div className={header}>
-        <img src="../../assets/arrow-left.png" />
-        <h1 className={header_title}>Checkout</h1>
+        <img src="../../assets/arrow-left.png" alt="left arrow" />
+        <h1 className={headerTitle}>Checkout</h1>
       </div>
 
-      <div className={shipping_info_container}>
-        <div className={shipping_info_header}>
+      <div className={shippingInfoContainer}>
+        <div className={shippingInfoHeader}>
           <h2>Shiping Information</h2>
           <span>change</span>
         </div>
-        <div className={shipping_info_main}>
-          <div className={shipping_info_item}>
-            <img src="../../assets/profile.png" width="18px" height="auto" />
+        <div className={shippingInfoMain}>
+          <div className={shippingInfoItem}>
+            <img
+              src="../../assets/profile.png"
+              width="18px"
+              height="auto"
+              alt="profile"
+            />
             <p>Rosina Doe</p>
           </div>
-          <div className={shipping_info_item}>
-            <img src="../../assets/location.png" width="20px" height="auto" />
+          <div className={shippingInfoItem}>
+            <img
+              src="../../assets/location.png"
+              width="20px"
+              height="auto"
+              alt="address"
+            />
             <p>43 Oxford Road M13 4GR Manchester, UK</p>
           </div>
-          <div className={shipping_info_item}>
-            <img src="../../assets/call.png" width="20px" height="auto" />
+          <div className={shippingInfoItem}>
+            <img
+              src="../../assets/call.png"
+              width="20px"
+              height="auto"
+              alt="phone"
+            />
             <p>+234 9011039271</p>
           </div>
         </div>
       </div>
 
-      <div className={payment_info_container}>
-        <h2 className={payment_info_header}>Payment Method</h2>
-        <div className={payment_info_main}>
-          <div className={payment_info_option}>
+      <div className={paymentInfoContainer}>
+        <h2 className={paymentInfoHeader}>Payment Method</h2>
+        <div className={paymentInfoMain}>
+          <div className={paymentInfoOption}>
             <input type="radio" name="payment-card" />
-            <div className={payment_info_card_wrapper}>
-              <img src="../../assets/visa.png" width="40px" height="auto" />
+            <div className={paymentInfoCardWrapper}>
+              <img
+                src="../../assets/visa.png"
+                width="40px"
+                height="auto"
+                alt="visa"
+              />
             </div>
             <span>**** **** **** 1234</span>
           </div>
-          <div className={payment_info_option}>
+          <div className={paymentInfoOption}>
             <input type="radio" name="payment-card" />
-            <div className={payment_info_card_wrapper}>
+            <div className={paymentInfoCardWrapper}>
               <img
+                alt="mastercard"
                 src="../../assets/master-card.png"
                 width="40px"
                 height="auto"
@@ -76,9 +96,9 @@ export default function Checkout() {
         </div>
       </div>
 
-      <div className={payment_total}>
+      <div className={paymentTotal}>
         <span>Total</span>
-        <span className={payment_total_amount}>$954</span>
+        <span className={paymentTotalAmount}>$954</span>
       </div>
 
       <Button inverted>Confirm and Pay</Button>
