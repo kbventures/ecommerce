8b737e71c3d8cbd78aad3118dbeee18213c80f2a
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-07-31 17:42:23 +0800

    Refactor checkout page

--- src/routes/Checkout/index.jsx
+++ src/routes/Checkout/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { string, number } from "prop-types";
 import styles from "./checkout.module.css";
 import Container from "../../components/Container";
 import Button from "../../components/Button";
@@ -23,78 +24,104 @@ export default function Checkout() {
   return (
     <Container backgroundColor="rgb(245, 245, 248)">
       <Header title="Checkout" />
-
-      <div className={shippingInfoContainer}>
-        <div className={shippingInfoHeader}>
-          <h2>Shiping Information</h2>
-          <span>change</span>
-        </div>
-        <div className={shippingInfoMain}>
-          <div className={shippingInfoItem}>
-            <img
-              src="../../assets/profile.png"
-              width="18px"
-              height="auto"
-              alt="profile"
-            />
-            <p>Rosina Doe</p>
-          </div>
-          <div className={shippingInfoItem}>
-            <img
-              src="../../assets/location.png"
-              width="20px"
-              height="auto"
-              alt="shipping address"
-            />
-            <p>43 Oxford Road M13 4GR Manchester, UK</p>
-          </div>
-          <div className={shippingInfoItem}>
-            <img
-              src="../../assets/call.png"
-              width="20px"
-              height="auto"
-              alt="telephone"
-            />
-            <p>+234 9011039271</p>
-          </div>
-        </div>
-      </div>
-
-      <div className={paymentInfoContainer}>
-        <h2 className={paymentInfoHeader}>Payment Method</h2>
-        <div className={paymentInfoMain}>
-          <div className={paymentInfoOption}>
-            <input type="radio" name="payment-card" />
-            <div className={paymentInfoCardWrapper}>
-              <img
-                src="../../assets/visa.png"
-                width="40px"
-                height="auto"
-                alt="visa"
-              />
-            </div>
-            <span>**** **** **** 1234</span>
-          </div>
-          <div className={paymentInfoOption}>
-            <input type="radio" name="payment-card" />
-            <div className={paymentInfoCardWrapper}>
-              <img
-                alt="mastercard"
-                src="../../assets/master-card.png"
-                width="40px"
-                height="auto"
-              />
-            </div>
-            <span>**** **** **** 1234</span>
-          </div>
-        </div>
-      </div>
-
+      <ShippingInfo
+        name="Rosina Doe"
+        address="43 Oxford Road M13 4GR Manchester, UK"
+        phoneNumber={+2349011039271}
+      />
+      <PaymentMethod
+        visaCardNumber="**** **** **** 1234"
+        masterCardNumber="**** **** **** 1234"
+      />
       <div className={totalPriceWrapper}>
         <TotalPrice amount={954} />
       </div>
-
       <Button inverted>Confirm and Pay</Button>
     </Container>
   );
 }
+
+function PaymentMethod({ visaCardNumber, masterCardNumber }) {
+  return (
+    <div className={paymentInfoContainer}>
+      <h2 className={paymentInfoHeader}>Payment Method</h2>
+      <div className={paymentInfoMain}>
+        <div className={paymentInfoOption}>
+          <input type="radio" name="payment-card" />
+          <div className={paymentInfoCardWrapper}>
+            <img
+              src="../../assets/visa.png"
+              width="40px"
+              height="auto"
+              alt="visa"
+            />
+          </div>
+          <span>{visaCardNumber}</span>
+        </div>
+        <div className={paymentInfoOption}>
+          <input type="radio" name="payment-card" />
+          <div className={paymentInfoCardWrapper}>
+            <img
+              alt="mastercard"
+              src="../../assets/master-card.png"
+              width="40px"
+              height="auto"
+            />
+          </div>
+          <span>{masterCardNumber}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ShippingInfo({ name, address, phoneNumber }) {
+  return (
+    <div className={shippingInfoContainer}>
+      <div className={shippingInfoHeader}>
+        <h2>Shiping Information</h2>
+        <span>change</span>
+      </div>
+      <div className={shippingInfoMain}>
+        <div className={shippingInfoItem}>
+          <img
+            src="../../assets/profile.png"
+            width="18px"
+            height="auto"
+            alt="profile"
+          />
+          <p>{name}</p>
+        </div>
+        <div className={shippingInfoItem}>
+          <img
+            src="../../assets/location.png"
+            width="20px"
+            height="auto"
+            alt="shipping address"
+          />
+          <p>{address}</p>
+        </div>
+        <div className={shippingInfoItem}>
+          <img
+            src="../../assets/call.png"
+            width="20px"
+            height="auto"
+            alt="telephone"
+          />
+          <p>{phoneNumber}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+PaymentMethod.propTypes = {
+  visaCardNumber: string.isRequired,
+  masterCardNumber: string.isRequired,
+};
+
+ShippingInfo.propTypes = {
+  name: string.isRequired,
+  address: string.isRequired,
+  phoneNumber: number.isRequired,
+};
