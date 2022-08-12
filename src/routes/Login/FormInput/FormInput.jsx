8942b88d7eed5815f7542d711aee0d289498b396
Andersen <rickansenn@gmail.com>
2022-08-12 18:41:59 +0800

    refactor: Refactored login-page. Renamed form comp to FormInput and nested it inside new comp LoginForm. Also updated styling and removed unnecessary parent divs

--- src/routes/Login/FormInput/FormInput.jsx
+++ src/routes/Login/FormInput/FormInput.jsx
@@ -1,32 +1,30 @@
 import React from "react";
 import { string } from "prop-types";
-import styles from "./Form.module.css";
+import styles from "./FormInput.module.css";
 
 import EmailSvg from "../../../../public/assets/Message.svg";
 import PasswordSvg from "../../../../public/assets/Lock.svg";
 
-export default function Form({ children, type }) {
-  const { labelSvg, formContainer, label } = styles;
+export default function FormInput({ children, type }) {
+  const { labelSvg, formInputContainer, label } = styles;
   const icon = {
     email: <EmailSvg className={labelSvg} />,
     password: <PasswordSvg className={labelSvg} />,
   };
 
   return (
-    <div>
-      <label htmlFor={type} className={formContainer}>
-        <div className={label}>
-          {icon[type]}
-          {children}
-        </div>
+    <label htmlFor={type} className={formInputContainer}>
+      <div className={label}>
+        {icon[type]}
+        {children}
+      </div>
 
-        <input id={type} type={type} />
-      </label>
-    </div>
+      <input id={type} type={type} />
+    </label>
   );
 }
 
-Form.propTypes = {
+FormInput.propTypes = {
   children: string.isRequired,
   type: string.isRequired,
 };
--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -5,7 +5,7 @@ import EmptySmallEllipseSvg from "../../../public/assets/Ellipse 22.svg";
 import SolidPinkEllipseSvg from "../../../public/assets/Ellipse 19 pink.svg";
 
 import Button from "../../components/Button";
-import Form from "./Form/Form.index";
+import LoginForm from "./LoginForm/LoginForm";
 
 const {
   container,
@@ -17,48 +17,41 @@ const {
   header,
   overlay,
   loginHeader,
-  formMargin,
   passcodeLink,
   createAccountLink,
 } = styles;
 
 export default function Login() {
   return (
-    <div>
-      <div className={container}>
-        <section className={top}>
-          <EmptySmallEllipseSvg className={EmptySmallEllipsePositionOne} />
+    <div className={container}>
+      <section className={top}>
+        <EmptySmallEllipseSvg className={EmptySmallEllipsePositionOne} />
 
-          <EmptySmallEllipseSvg className={EmptySmallEllipsePositionTwo} />
+        <EmptySmallEllipseSvg className={EmptySmallEllipsePositionTwo} />
 
-          <div>
-            <SolidPinkEllipseSvg className={SolidPinkEllipsePosition} />
-            <div className={transparentSvg} />
-          </div>
+        <div>
+          <SolidPinkEllipseSvg className={SolidPinkEllipsePosition} />
+          <div className={transparentSvg} />
+        </div>
 
-          <h1 className={header}>Welcome Back</h1>
-        </section>
+        <h1 className={header}>Welcome Back</h1>
+      </section>
 
-        <section className={overlay}>
-          <h3 className={loginHeader}>Login</h3>
+      <section className={overlay}>
+        <h3 className={loginHeader}>Login</h3>
 
-          <form>
-            <div className={formMargin}>
-              <Form type="email">Email</Form>
-            </div>
-            <Form type="password">Password</Form>
-          </form>
+        <LoginForm />
 
-          <a href="/home" className={passcodeLink}>
-            Forgot passcode?
-          </a>
-          <Button inverted>Login</Button>
+        <a href="/home" className={passcodeLink}>
+          Forgot passcode?
+        </a>
 
-          <a href="/home" className={createAccountLink}>
-            Create account
-          </a>
-        </section>
-      </div>
+        <Button inverted>Login</Button>
+
+        <a href="/home" className={createAccountLink}>
+          Create account
+        </a>
+      </section>
     </div>
   );
 }
