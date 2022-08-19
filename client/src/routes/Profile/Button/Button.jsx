8d794cfa6c111c29e6fba09f6dd30ff0e5018e32
Andersen <rickansenn@gmail.com>
2022-08-19 22:06:24 +0800

    Completed profile page with media queries

---
+++ client/src/routes/Profile/Button/Button.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { string } from "prop-types";
+import styles from "./Button.module.css";
+
+import RightArrowHead from "../../../../public/assets/chevron-left.svg";
+
+export default function Button({ title }) {
+    return (
+        <section className={styles.button}>
+            {title}
+            <RightArrowHead className={styles.svg} />
+        </section>
+    );
+}
+
+Button.propTypes = {
+    title: string.isRequired,
+};
