7cddb7529f9983845b57adb1c1baa0af64397a42
Andersen <rickansenn@gmail.com>
2022-08-21 17:10:19 +0800

    refactor: updated items context to fetch data from the new database in server

--- client/src/contexts/ItemsContext.jsx
+++ client/src/contexts/ItemsContext.jsx
@@ -1,94 +1,96 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 // data in our store
-const items = [
-  {
-    id: "",
-    inventory: 100,
-    title: "Super Long Watch Name",
-    desc: "Series 6. Red",
-    longDesc:
-      "Available when you purchase any new iPhone, iPad, iPod Touch, Mac or Apple TV, 4.99/month after free trial.",
-    fullDesc: "",
-    color: [
-      ["Sky Blue", "#80BDE3"],
-      ["Rose Gold", "#B76E79"],
-      ["green", "#9FE2BF"],
-    ],
-    price: 359,
-    src: "assets/apple-watch-red.png",
-    promo: "",
-  },
-  {
-    id: "",
-    inventory: 100,
-    title: "SAMSUNG Galaxy Watch",
-    desc: "Active. Green",
-    longDesc:
-      "Available when you purchase any new iPhone, iPad, iPod Touch, Mac or Apple TV, 4.99/month after free trial.",
-    fullDesc: "",
-    color: [
-      ["Sky Blue", "#80BDE3"],
-      ["Rose Gold", "#B76E79"],
-      ["green", "#9FE2BF"],
-    ],
-    price: 159,
-    src: "assets/samsung-galaxy-watch.png",
-    promo: "",
-  },
-  {
-    id: "",
-    inventory: 100,
-    title: "SAMSUNG Galaxy Watch",
-    desc: "Active. Green",
-    longDesc:
-      "Available when you purchase any new iPhone, iPad, iPod Touch, Mac or Apple TV, 4.99/month after free trial.",
-    fullDesc: "",
-    color: [
-      ["Sky Blue", "#80BDE3"],
-      ["Rose Gold", "#B76E79"],
-      ["green", "#9FE2BF"],
-    ],
-    price: 159,
-    src: "assets/samsung-galaxy-watch.png",
-    promo: "",
-  },
-  {
-    id: "",
-    inventory: 100,
-    title: "SAMSUNG Galaxy Watch",
-    desc: "Active. Green",
-    longDesc:
-      "Available when you purchase any new iPhone, iPad, iPod Touch, Mac or Apple TV, 4.99/month after free trial.",
-    fullDesc: "",
-    color: [
-      ["Sky Blue", "#80BDE3"],
-      ["Rose Gold", "#B76E79"],
-      ["green", "#9FE2BF"],
-    ],
-    price: 159,
-    src: "assets/samsung-galaxy-watch.png",
-    promo: "",
-  },
-  {
-    id: "",
-    inventory: 100,
-    title: "SAMSUNG Galaxy Watch",
-    desc: "Active. Green",
-    longDesc:
-      "Available when you purchase any new iPhone, iPad, iPod Touch, Mac or Apple TV, 4.99/month after free trial.",
-    fullDesc: "",
-    color: [
-      ["Sky Blue", "#80BDE3"],
-      ["Rose Gold", "#B76E79"],
-      ["green", "#9FE2BF"],
-    ],
-    price: 159,
-    src: "assets/samsung-galaxy-watch.png",
-    promo: "",
-  },
-];
+
+
+// const items = [
+//   {
+//     id: "",
+//     inventory: 100,
+//     title: "Super Long Watch Name",
+//     desc: "Series 6. Red",
+//     longDesc:
+//       "Available when you purchase any new iPhone, iPad, iPod Touch, Mac or Apple TV, 4.99/month after free trial.",
+//     fullDesc: "",
+//     color: [
+//       ["Sky Blue", "#80BDE3"],
+//       ["Rose Gold", "#B76E79"],
+//       ["green", "#9FE2BF"],
+//     ],
+//     price: 359,
+//     src: "assets/apple-watch-red.png",
+//     promo: "",
+//   },
+//   {
+//     id: "",
+//     inventory: 100,
+//     title: "SAMSUNG Galaxy Watch",
+//     desc: "Active. Green",
+//     longDesc:
+//       "Available when you purchase any new iPhone, iPad, iPod Touch, Mac or Apple TV, 4.99/month after free trial.",
+//     fullDesc: "",
+//     color: [
+//       ["Sky Blue", "#80BDE3"],
+//       ["Rose Gold", "#B76E79"],
+//       ["green", "#9FE2BF"],
+//     ],
+//     price: 159,
+//     src: "assets/samsung-galaxy-watch.png",
+//     promo: "",
+//   },
+//   {
+//     id: "",
+//     inventory: 100,
+//     title: "SAMSUNG Galaxy Watch",
+//     desc: "Active. Green",
+//     longDesc:
+//       "Available when you purchase any new iPhone, iPad, iPod Touch, Mac or Apple TV, 4.99/month after free trial.",
+//     fullDesc: "",
+//     color: [
+//       ["Sky Blue", "#80BDE3"],
+//       ["Rose Gold", "#B76E79"],
+//       ["green", "#9FE2BF"],
+//     ],
+//     price: 159,
+//     src: "assets/samsung-galaxy-watch.png",
+//     promo: "",
+//   },
+//   {
+//     id: "",
+//     inventory: 100,
+//     title: "SAMSUNG Galaxy Watch",
+//     desc: "Active. Green",
+//     longDesc:
+//       "Available when you purchase any new iPhone, iPad, iPod Touch, Mac or Apple TV, 4.99/month after free trial.",
+//     fullDesc: "",
+//     color: [
+//       ["Sky Blue", "#80BDE3"],
+//       ["Rose Gold", "#B76E79"],
+//       ["green", "#9FE2BF"],
+//     ],
+//     price: 159,
+//     src: "assets/samsung-galaxy-watch.png",
+//     promo: "",
+//   },
+//   {
+//     id: "",
+//     inventory: 100,
+//     title: "SAMSUNG Galaxy Watch",
+//     desc: "Active. Green",
+//     longDesc:
+//       "Available when you purchase any new iPhone, iPad, iPod Touch, Mac or Apple TV, 4.99/month after free trial.",
+//     fullDesc: "",
+//     color: [
+//       ["Sky Blue", "#80BDE3"],
+//       ["Rose Gold", "#B76E79"],
+//       ["green", "#9FE2BF"],
+//     ],
+//     price: 159,
+//     src: "assets/samsung-galaxy-watch.png",
+//     promo: "",
+//   },
+// ];
 
 // Old Data Version
 // const items = [
@@ -138,12 +140,27 @@ export function useItems() {
 
 // Provides the store to your application
 export function ItemsProvider({ children }) {
+  const [items, setItems] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:4001/items')
+      .then(res => {
+        return res.json()
+      })
+      .then((data) => {
+        setItems(data)
+      })
+  }, [])
+
   return (
     // wrap our React components with our store provider (store has "knowledge of your app")
-    <ItemsContext.Provider value={items}>
-      {/* eventually "children" will be our react app */}
-      {children}
-    </ItemsContext.Provider>
+    <div>
+      {items && <ItemsContext.Provider value={items}>
+        {/* eventually "children" will be our react app */}
+        {children}
+      </ItemsContext.Provider>}
+    </div>
+
   );
 }
 
