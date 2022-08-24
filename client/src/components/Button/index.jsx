71693baa838992d69ae383bbf180677335c05e4e
Dmitry Kulakov <atomeistee@gmail.com>
2022-08-24 13:26:40 +0300

    Add ability to change button type by passing type prop

--- client/src/components/Button/index.jsx
+++ client/src/components/Button/index.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./button.module.css";
 
-export default function Button({ children, inverted }) {
+export default function Button({ children, inverted, type }) {
   return (
     <button
-      type="button"
+      // eslint-disable-next-line react/button-has-type
+      type={type}
       style={{
         backgroundColor: inverted ? "#5956e9" : "white",
         color: inverted ? "white" : "#5956e9",
@@ -20,8 +21,10 @@ export default function Button({ children, inverted }) {
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   inverted: PropTypes.bool,
+  type: PropTypes.string,
 };
 
 Button.defaultProps = {
   inverted: false,
+  type: "button",
 };
