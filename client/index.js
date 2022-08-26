d47df86b9c65be71c2150043a5553aaae9b037ad
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-08-26 21:34:02 +0800

    Added env file to client folder

--- client/index.js
+++ client/index.js
@@ -1,19 +1,19 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, "dist")));
 
 // serve our React application for all GET request paths
 // change to test deployment
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log('listening on port ', PORT);
+  console.log("listening on port ", PORT);
 });
 
 // Hello World
