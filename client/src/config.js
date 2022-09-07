fcdc7c6c6494d6292ca70dda3fb3751e33a01df2
kbventures <kmb_x@hotmail.com>
2022-09-07 10:36:48 -0600

    test

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,7 @@
 export const BACKEND_URL =
-process.env.NODE_ENV === 'development'
-  ? 'http://localhost:4001'
-  : `${process.env.BACKEND_URL}`;
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:4001'
+    : `${process.env.BACKEND_URL}`;
 
+console.log(process.env.NODE_ENV);
+console.log(process.env.BACK_URL);
