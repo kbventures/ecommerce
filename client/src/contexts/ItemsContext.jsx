b3b1c6d38f36d24799395c03a84f11881aa78a24
Ken Beaudin <30326135+kbventures@users.noreply.github.com>
2022-11-24 16:41:09 -0600

    Update ItemsContext.jsx

--- client/src/contexts/ItemsContext.jsx
+++ client/src/contexts/ItemsContext.jsx
@@ -20,7 +20,8 @@ export function ItemsProvider({ children }) {
   useEffect(() => {
     const fetchItems = async () => {
       const response = await fetch(
-        `https://kdaa-ecommerce-back-end.herokuapp.com/products`
+        `https://kdaa-ecommerce-back-end.herokuapp.com/products`,
+        { method: "GET" }
       );
       const json = await response.json();
       setItems(json);
