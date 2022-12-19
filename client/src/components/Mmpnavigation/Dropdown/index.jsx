d0e1937549001f12fe2c0094bf916cf098aaca38
kbventures <kmb_x@hotmail.com>
2022-12-19 11:57:09 -0600

    Refactored and added Proptypes to Navbar drop down menu

---
+++ client/src/components/Mmpnavigation/Dropdown/index.jsx
@@ -0,0 +1,42 @@
+import React, { useState, cloneElement } from "react";
+import PropTypes from "prop-types";
+
+export default function Dropdown({ trigger, menu }) {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className="dropdown">
+      {cloneElement(trigger, {
+        onClick: handleOpen,
+      })}
+      {open ? (
+        <ul className="menu">
+          {menu.map((menuItem, index) => (
+            <li key={index} className="menu-item">
+              {cloneElement(menuItem, {
+                onClick: () => {
+                  menuItem.props.onClick();
+                  setOpen(false);
+                },
+              })}
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
+
+Dropdown.propTypes = {
+  trigger: PropTypes.node.isRequired,
+  menu: PropTypes.arrayOf(
+    PropTypes.shape({
+      menuItem: PropTypes.element.isRequired,
+      index: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
--- client/src/components/Mmpnavigation/index.jsx
+++ client/src/components/Mmpnavigation/index.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navigation.module.css";
-import Hamburger from "./Hamburger";
+import Dropdown from "./Dropdown";
 
 export default function Navigation() {
+  const handleMenuOne = () => {
+    console.log("clickedOne");
+  };
+
+  const handleMenuTwo = () => {
+    console.log("clicked two");
+  };
+
   return (
     <ul className={styles.container}>
-      <Hamburger />
+      <Dropdown
+        trigger={<button type="button">Dropdown</button>}
+        menu={[
+          <button type="button" onClick={handleMenuOne}>
+            Menu 1
+          </button>,
+          <button type="button" onClick={handleMenuTwo}>
+            Menu 2
+          </button>,
+        ]}
+      />
       <Link to="/">eRenaissance</Link>
       <Link to="/profile">Profile Icon</Link>
       <Link to="/basket">Basket Icon</Link>
